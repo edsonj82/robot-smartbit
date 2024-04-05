687de4ac608c251fa45c0469b159725146fbdeeb
Edson José dos Santos <edsonj82@outlook.com>
2024-04-05 16:56:56 -0300

    Implementing faker with python for the scenario 'Should show the register user'

--- resources/libs/Account.py
+++ resources/libs/Account.py
@@ -1,7 +1,11 @@
+from faker import Faker
+
+fake = Faker('pt_BR')
+
 def get_fake_account():
     account = {
-        "name": "Edson José",
-        "email": "xpto@gmail.com",
-        "document":"99611793073"
+        "name": fake.name(),
+        "email": fake.email(),
+        "document":fake.cpf()
     }
     return account
